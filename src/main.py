06264f835a8d7d526479b3ca79ad4f5f99d4468d
Alfred Jophy <alfredjophy@protonmail.com>
2022-01-18 16:43:46 +0530

    added support for multiple browsers

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ import subprocess
     # }''')
 
 # config = json.loads(config_file)
+# should represent the domain/IP ,else have full regex
 
 config = [
     {
@@ -28,28 +29,58 @@ config = [
         'url_patterns' : ['google.com','meet.google.com','forms.google.com'],
         'browser' : 'firefox',
         'browser_profile' : 'college_stuff'
+    },
+    {
+        'url_patterns': ['algata.ga'],
+        'browser' : 'brave',
+        'browser_profile' : 'Profile 13'
+    },
+    {
+        'url_patterns': ['github.com'],
+        'browser' : 'firefoxpwa',
+        'browser_profile' : '01FFAZW39PG9F70N1ASEZ17G5K'
     }
 ]
 
 url = sys.argv[1]
 
+browser_defaults = {
+    "chromium" : {
+        "browsers" : ["brave","chromium","google-chrome"],
+        "profile_command" : "--profile_directory=",
+    },
+    "firefox" : {
+        "browsers": ["firefox","icecat","librewolf"],
+        "profile_command": "-P",
+    },
+    "firefox_pwa": {
+        "browsers" : ["firefoxpwa"],
+        "profile_command" : "site launch",
+    }
+}
+
 def launch_command(config,url) : 
-    command = "{} -P {} {}".format(config["browser"],config["browser_profile"],url)
+    command = config["browser"]
+
+    for i in browser_defaults:
+        if config["browser"] in browser_defaults[i]["browsers"]:
+            command = "{} {} '{}' {}".format(command,browser_defaults[i]["profile_command"],config["browser_profile"],url)
+            break
+
+    print(command)
     return command
 
-process = ""
 for i in config:
     for j in i["url_patterns"]:
         if re.match(j,url,re.I) :
             command=launch_command(i,url)
-            process = subprocess.Popen(command,stdout=subprocess.DEVNULL,start_new_session=True,stderr=subprocess.STDOUT,shell=True)
+            process = subprocess.Popen(command,start_new_session=True,stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT,shell=True)
+            process.communicate() 
             break
     else:
         continue
     break
 
-
-process.communicate() 
 exit()
             
 
